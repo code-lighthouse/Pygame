e121dacfb164607b8d90c8a0a6cadeb0cf0275bf
Cristiano Araujo <code.lighthouse@gmail.com>
2014-08-17 16:11:51 -0300

    Update tank.py

--- tank_game/tank.py
+++ tank_game/tank.py
@@ -1,25 +1,23 @@
 #! /usr/bin/python
 
+
 class Tank(object):
-    
     def __init__(self, name):
-       
         self.name = name
         self.alive = True
         self.ammo = 5
         self.armor = 60
 
-   
     def __str__(self):
 
         if self.alive:
-            return "%s (%i armor, %i shells)" % (self.name, self.armor, self.ammo)
-        else:
-            return "%s (DEAD)" % self.name
+            return "%s (%i armor, %i shells)" % \
+                (self.name, self.armor, self.ammo)
 
+        else:
+            return "%s(DEAD)" % self.name
 
     def fire_at(self, enemy):
-
         if self.ammo >= 1:
             self.ammo -= 1
             print self.name, "fires on", enemy.name
@@ -28,13 +26,11 @@ class Tank(object):
             print self.name, "has no shells!"
 
     def hit(self):
-
         self.armor -= 20
         print self.name, "is hit!"
         if self.armor <= 0:
             self.explode()
 
     def explode(self):
-
         self.alive = False
         print self.name, "explodes!"
